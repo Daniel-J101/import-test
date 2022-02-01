ebbbb29486c4601df37365e54bd1a7b47ed7615c
nandji <frostysausage123@gmail.com>
2022-01-31 19:01:13 -0600

    Added movies.txt

--- src/models/Store.java
+++ src/models/Store.java
@@ -1,4 +1,12 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Store {
+
+    private ArrayList<Movie> movies;
+
+    public Store() {
+        this.movies = new ArrayList<>();
+    }
 }
